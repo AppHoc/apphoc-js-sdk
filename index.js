aec33847b54223ae85b49a7828dfb841ec7f8be4
EricHier <developer.erichier@gmail.com>
2021-04-14 18:13:08 +0200

    adding further debug output and request Story method

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+function log(...args) {
+    console.log("APPHOC-SDK", ...args);
+}
+
 const AppHoc = {
 
     callbackMethod: () => {},
     debug: false,
 
     setCallbackMethod(callbackMethod) {
+        if (this.debug)
+            log("Setting callback method")
+
         this.callbackMethod = callbackMethod;
     },
 
-    recieveBeaconEvent(beaconData) {
+    _receiveBeaconEvent(beaconData) {
         if (this.debug)
-            console.log("Recieved new beacon data", beaconData);
+            log("Received new beacon data", beaconData);
 
         this.callbackMethod(beaconData);
+    },
+
+    requestStory(url) {
+        try {
+            if (this.debug)
+                log("Requesting story", url);
+
+            AppHocFlutter.postMessage(`STORY ${url}`);
+        } catch (e) {
+            if (this.debug)
+                log("Got error while requesting story");
+
+            throw new Error("SDK not running in AppHoc");
+        }
     }
 
 };
